dad671afb069f523922c0bcf96665bbce742787f
Anurag Patil <aspanurag@gmail.com>
2020-02-06 21:06:21 -0500

    Collection View

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-import { UserForm } from "./views/UserForm";
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { User, UserProps } from "./models/User";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
 
-const user = User.buildUser({ name: "NAME", age: 20 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.querySelector("#root");
+users.on("change", () => {
+  const root = document.querySelector("#root");
+  if (root) {
+    new UserList(root, users).render();
+  } else {
+    throw "Root element not found";
+  }
+});
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw "Root element not found";
-}
+users.fetch();
